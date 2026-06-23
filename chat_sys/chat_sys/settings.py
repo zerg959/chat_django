c790e14905d0c5daf819599d5eb125f3f140f9d9
zerg959 <zerg959@yandex.ru>
2026-06-23 18:04:05 +0300

    fix: CORS

--- chat_sys/chat_sys/settings.py
+++ chat_sys/chat_sys/settings.py
@@ -52,6 +52,19 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://hub959.ru",
+    "http://hub959.ru",
+    "https://109.68.212.60",
+    "http://109.68.212.60",
+
+]
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",  # Обязательно для preflight
+    "PATCH",    # Обязательно для вашего случая
+    "POST",
+    "PUT",
 ]
 
 MIDDLEWARE = [
